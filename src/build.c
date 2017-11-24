be1e740eb25bc67a09b97358438758bb9d042b75
BuildTools <unconfigured@null.spigotmc.org>
2017-11-24 11:00:58 -0600

    fixing skinning

--- src/build.c
+++ src/build.c
@@ -1455,10 +1455,8 @@ void do_mset( CHAR_DATA * ch, char *argument )
          return;
       }
       victim->skinamount = value;
-	  victim->curr_skinamount = value;
       if( IS_NPC( victim ) && xIS_SET( victim->act, ACT_PROTOTYPE ) )
          victim->pIndexData->skinamount = value;
-		 victim->pIndexData->curr_skinamount = value;
       return;
    }
    

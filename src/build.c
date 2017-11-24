b9209f0a7c33c167dadabe02f1ae48f783eda637
BuildTools <unconfigured@null.spigotmc.org>
2017-11-24 09:32:32 -0600

    adding CARVINGKNIFE item type

--- src/build.c
+++ src/build.c
@@ -1116,8 +1116,6 @@ void do_mset( CHAR_DATA * ch, char *argument )
       victim->skinamount = value;
       victim->curr_skinamount = victim->skinamount;
       if( IS_NPC( victim ) && xIS_SET( victim->act, ACT_PROTOTYPE ) )
-         victim->pIndexData->skinamount = value;
-      if( IS_NPC( victim ) && xIS_SET( victim->act, ACT_PROTOTYPE ) )
          victim->pIndexData->curr_skinamount = value;
       return;
    }

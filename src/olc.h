2ebcfc137f8cb27a4c7f684ef9b3374e5d51cb4d
BuildTools <unconfigured@null.spigotmc.org>
2017-11-24 23:18:34 -0600

    fixing skinning

--- src/olc.h
+++ src/olc.h
@@ -237,7 +237,8 @@ struct olc_save_info
 #define MEDIT_SAV5			59
 #define MEDIT_CLASS			60
 #define MEDIT_RACE			61
-#define MEDIT_ENDURANCE		62
+#define MEDIT_ENDURANCE		        62
+#define MEDIT_SKIN                      63
 
 /*. Submodes of SEDIT connectedness 	.*/
 #define SEDIT_MAIN_MENU              	0

592a719c79f936e6d7a622c8e86880692cba0799
BuildTools <unconfigured@null.spigotmc.org>
2017-11-26 18:57:52 -0600

    apparently I have to add what skin type the creature has

--- src/build.c
+++ src/build.c
@@ -2735,7 +2735,7 @@ void do_oset( CHAR_DATA * ch, char *argument )
           }
           if( atoi( arg3 ) >= SKIN_MAX || atoi( arg3 ) < -1 )
           {
-              send_to_char( "That is not a valid skin type. \r\n", ch );
+              send_to_char( "That is not a valid hide type. \r\n", ch );
               obj->hidetype = 0;
               return;
           }
@@ -3342,7 +3342,7 @@ void do_material( CHAR_DATA * ch, char *argument )
       send_to_char( "Where <field> is one of:\r\n", ch );
       send_to_char( "  name short long weight flags\r\n", ch );
       send_to_char( "  affect sector race rarity\r\n", ch );
-      send_to_char( " &GSkin Type\r\n", ch);
+      send_to_char( " &GHideType\r\n", ch);
       send_to_char( "     &G1)Scraps,    2)Scrawny Pelt, 3)Scrawny Fur, 4)Scrawny Hide, 5)Rough Pelt, 6)Rough Fur\r\n", ch );
       send_to_char( "     &G7)Rough Hide, 8)Thin Pelt, 9)Thin Fur, 10)Thin Hide, 11)Pelt, 12)Fur, 13)Hide, 14)Tough Pelt\r\n", ch );
       send_to_char( "    &G15)Tough Fur, 16)Tough Hide, 17)Fine Pelt, 18)Fine Fur, 19)Fine Hide, 20)Fancy Pelt\r\n", ch );
